c30097b284fc9996c1d2e081b0768840960fea63
Abdellatif Sraiti <abdellsraiti019@gmail.com>
2024-04-01 22:58:44 +0000

    removed logs and did some refactoring

--- app/main.js
+++ app/main.js
@@ -2,7 +2,6 @@ const { readFile, writeFile } = require("fs/promises");
 const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
 
 const args = process.argv;
 
@@ -14,29 +13,14 @@ function getArgValue() {
 const server = net.createServer((socket) => {
   socket.on("data", async (data) => {
     const request = data.toString();
-
     const reqParts = request.split("\r\n");
-
-    const requestVerb = reqParts[0].split(" ")[0];
-    console.log({ reqParts });
+    const [requestVerb, path] = reqParts[0].split(" ")[0];
 
     const userAgentHeader = reqParts.find((part) =>
       part.startsWith("User-Agent")
     );
 
-    console.log({
-      userAgentHeader,
-    });
-
     const userAgent = userAgentHeader ? userAgentHeader.split(": ")[1] : null;
-
-    console.log({
-      userAgent,
-    });
-
-    const path = reqParts[0].split(" ")[1];
-
-    console.log({ path });
 
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
@@ -44,20 +28,12 @@ const server = net.createServer((socket) => {
 
     if (path.startsWith("/echo")) {
       const echoParam = path.substring(6);
-
-      console.log({
-        echoParam,
-        path,
-      });
-
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoParam.length}\r\n\r\n${echoParam}`
       );
     }
 
     if (path.startsWith("/user-agent") && userAgent) {
-      console.log({ userAgent });
-
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
       );
@@ -67,8 +43,6 @@ const server = net.createServer((socket) => {
       const directory = getArgValue();
 
       const fileName = path.split("/").pop();
-
-      console.log({ directory, fileName });
 
       const fileContent = await readFile(
         `${directory}${fileName}`,
@@ -89,7 +63,6 @@ const server = net.createServer((socket) => {
       const fileName = path.split("/").pop();
 
       const payload = reqParts[reqParts.length - 1];
-      console.log({ directory, fileName });
 
       await writeFile(`${directory}${fileName}`, payload).catch((err) => {
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
