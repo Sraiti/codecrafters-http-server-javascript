4e6339733be00cb296cc49aac18cf7046d074891
Abdellatif Sraiti <abdellsraiti019@gmail.com>
2024-04-01 21:24:15 +0000

    pass stage

--- app/main.js
+++ app/main.js
@@ -6,8 +6,17 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    const request = data.toString();
+
+    const path = request.split(" ")[1];
+
+    if (path === "/") {
+      return socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    }
+
+    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
   });
+
   socket.on("close", () => {
     socket.end();
     server.close();
