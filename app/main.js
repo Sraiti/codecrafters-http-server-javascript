103feab31a7935fe3c17ac61342f2421694aa5bc
Abdellatif Sraiti <abdellsraiti019@gmail.com>
2024-04-01 22:44:27 +0000

    stage passed

--- app/main.js
+++ app/main.js
@@ -1,11 +1,18 @@
+const { readFile } = require("fs/promises");
 const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const args = process.argv;
+
+function getArgValue() {
+  return args[args.findIndex((arg) => arg === "--directory") + 1];
+}
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data", (data) => {
+  socket.on("data", async (data) => {
     const request = data.toString();
 
     const reqParts = request.split("\r\n");
@@ -28,18 +35,22 @@ const server = net.createServer((socket) => {
 
     const path = reqParts[0].split(" ")[1];
 
-    const pathParam = path.substring(6);
-
-    console.log({ path, pathParam });
+    console.log({ path });
 
     if (path === "/") {
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
     }
 
     if (path.startsWith("/echo")) {
-      console.log("echo", pathParam);
+      const echoParam = path.substring(6);
+
+      console.log({
+        echoParam,
+        path,
+      });
+
       socket.write(
-        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${pathParam.length}\r\n\r\n${pathParam}`
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${echoParam.length}\r\n\r\n${echoParam}`
       );
     }
 
@@ -49,6 +60,28 @@ const server = net.createServer((socket) => {
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
       );
+    }
+
+    if (path.startsWith("/files")) {
+      const directory = getArgValue();
+
+      const fileName = path.split("/").pop();
+
+      console.log({ directory, fileName });
+
+      const fileContent = await readFile(
+        `${directory}${fileName}`,
+        "utf-8"
+      ).catch((err) => {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        socket.end();
+      });
+
+      if (fileContent) {
+        socket.write(
+          `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`
+        );
+      }
     }
 
     socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
